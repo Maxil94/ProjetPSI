5eb4c64112f231211003984eb84152835efc9e25
rapherz16 <raphael.herz@edu.devinci.fr>
2025-03-01 15:00:53 +0100

    Update Program.cs

--- PeojetPSI/PeojetPSI/Program.cs
+++ PeojetPSI/PeojetPSI/Program.cs
@@ -5,21 +5,21 @@ using System.IO;
 class Program
 {
     static void Main()
-    {
-        Graphe graphe = new Graphe();
-        graphe.Fichier("soc-karate.txt");
-        Noeud noeud1 = graphe.CreerNoeud(1);
-        Noeud noeud2 = graphe.CreerNoeud(2);
-        graphe.AjouterLien(noeud1, noeud2);
-        graphe.CréationGraphe();
+{
+    Graphe graphe = new Graphe();
+    graphe.Fichier("soc-karate.txt");
+    Noeud noeud1 = graphe.CreerNoeud(1);
+    Noeud noeud2 = graphe.CreerNoeud(2);
+    graphe.AjouterLien(noeud1, noeud2);
+    graphe.CréationGraphe();
+    Console.WriteLine("Parcours en profondeur:");
+    graphe.ParcoursProfondeur(noeud1, new HashSet<Noeud>());
+    Console.WriteLine("Parcours en largeur:");
+    graphe.ParcoursLargeur(noeud1);
 
-        ///Console.WriteLine("Parcours en profondeur:");
-        ///graphe.ParcoursProfondeur(1);
+    ///Console.WriteLine("Le graphe est-il connexe? " + graphe.EstConnexe());
+    ///Console.WriteLine("Le graphe contient-il un cycle? " + graphe.ContientCycle());
+}
 
-        ///Console.WriteLine("Parcours en largeur:");
-        ///graphe.ParcoursLargeur(1);
-
-        ///Console.WriteLine("Le graphe est-il connexe? " + graphe.EstConnexe());
-        ///Console.WriteLine("Le graphe contient-il un cycle? " + graphe.ContientCycle());
-    }
+    
 }
